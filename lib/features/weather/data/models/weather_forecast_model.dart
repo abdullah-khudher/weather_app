318b4d1c8973ced9ea010d17e413b768a1896682
abdullah-khudher <abdullahjacksi@gmail.com>
2025-06-29 16:16:07 +0200

    feat: implement weather forecast model with JSON parsing

---
+++ lib/features/weather/data/models/weather_forecast_model.dart
@@ -0,0 +1,49 @@
+class WeatherForecastModel {
+  final List<ForecastItemModel> forecasts;
+
+  WeatherForecastModel({required this.forecasts});
+
+  factory WeatherForecastModel.fromJson(Map<String, dynamic> json) {
+    return WeatherForecastModel(
+      forecasts: (json['list'] as List)
+          .map((item) => ForecastItemModel.fromJson(item))
+          .toList(),
+    );
+  }
+}
+
+class ForecastItemModel {
+  final DateTime dateTime;
+  final String weatherMain;
+  final String weatherDescription;
+  final String weatherIcon;
+  final double temperature;
+  final int humidity;
+  final int pressure;
+  final double windSpeed;
+
+  ForecastItemModel({
+    required this.dateTime,
+    required this.weatherMain,
+    required this.weatherDescription,
+    required this.weatherIcon,
+    required this.temperature,
+    required this.humidity,
+    required this.pressure,
+    required this.windSpeed,
+  });
+
+  factory ForecastItemModel.fromJson(Map<String, dynamic> json) {
+    final weather = json['weather'][0];
+    return ForecastItemModel(
+      dateTime: DateTime.parse(json['dt_txt']),
+      weatherMain: weather['main'],
+      weatherDescription: weather['description'],
+      weatherIcon: weather['icon'],
+      temperature: (json['main']['temp'] as num).toDouble(),
+      humidity: json['main']['humidity'],
+      pressure: json['main']['pressure'],
+      windSpeed: (json['wind']['speed'] as num).toDouble(),
+    );
+  }
+}
